bed136942a059599572b84c98a0458bc8d9fbff6
Zach Caldwell <zach.j.caldwell@gmail.com>
2020-06-28 08:42:25 -0700

    Add commands metadata file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Moda shade is running...");
+import { process } from 'process';
+
+process.on('message', message => {
+    console.log("message from process...");
+    console.log(message);
+});
